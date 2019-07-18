47dac8d0a38768b3b12609f513afae4ae52b7029
Alejo Wasser <alejowasser@gmail.com>
2019-07-18 01:30:16 -0300

    Landing Page

---
+++ layouts/default.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-app>
+        <v-toolbar dense flat fixed dark color="transparent">
+            <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>Callejeritos Almirante Brown</v-toolbar-title>
+        </v-toolbar>
+        <v-navigation-drawer v-model="drawer" :mini-variant="mini" absolute dark temporary>
+            <v-list class="pa-1">
+                <v-list-tile v-if="mini" @click.stop="mini = !mini">
+                    <v-list-tile-action>
+                        <v-icon>chevron_right</v-icon>
+                    </v-list-tile-action>
+                </v-list-tile>
+
+                <v-list-tile avatar tag="div">
+                    <v-list-tile-avatar>
+                        <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+                    </v-list-tile-avatar>
+
+                    <v-list-tile-content>
+                        <v-list-tile-title>John Leider</v-list-tile-title>
+                    </v-list-tile-content>
+
+                    <v-list-tile-action>
+                        <v-btn icon @click.stop="mini = !mini">
+                            <v-icon>chevron_left</v-icon>
+                        </v-btn>
+                    </v-list-tile-action>
+                </v-list-tile>
+            </v-list>
+
+            <!-- <v-list class="pt-0" dense>
+                <v-divider light></v-divider>
+
+                <v-list-tile v-for="item in items" :key="item.title" @click>
+                    <v-list-tile-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>-->
+        </v-navigation-drawer>
+        <nuxt />
+        <v-footer>
+            <span>&copy; 2019</span>
+        </v-footer>
+    </v-app>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: false,
+            mini: false
+        };
+    }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-container fluid pa-0>
+        <v-img src="https://cdn.wallpapersafari.com/23/39/Wy2uph.jpg"></v-img>
+        <v-layout row wrap px-5 align-start elevation-24 style="margin-top: -65px;">
+            <v-flex xs12>
+                <v-card class="pa-5 mx-auto">
+                    <v-card-title primary-title class="align-center justify-center">
+                        <div>
+                            <h3 class="display-1">Hablemos de Callejeritos</h3>
+                        </div>
+                    </v-card-title>
+                    <v-card-text>
+                        <p
+                            class="subheading text-xs-center"
+                        >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum minima illo sunt. Fugit, nobis praesentium incidunt aperiam officiis ratione exercitationem, dolorum commodi quo aut pariatur laboriosam, provident sunt consequatur. Debitis?Inventore tempora reprehenderit nam excepturi praesentium error voluptas a facere asperiores eligendi. Quas nobis accusantium eos itaque porro voluptate rerum, quia ab exercitationem cum omnis non, ducimus perferendis, iusto blanditiis.</p>
+                    </v-card-text>
+                    <v-container grid-list-xl>
+                        <v-layout row wrap>
+                            <v-flex xs4 class="text-xs-center">
+                                <v-icon size="65" class="mb-2">pets</v-icon>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sit quod id. Optio sint, earum quaerat laboriosam ab amet hic nesciunt, facilis exercitationem qui iusto ipsa impedit blanditiis cum reprehenderit?</p>
+                            </v-flex>
+                            <v-flex xs4 class="text-xs-center">
+                                <v-icon size="65" class="mb-2">pets</v-icon>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sit quod id. Optio sint, earum quaerat laboriosam ab amet hic nesciunt, facilis exercitationem qui iusto ipsa impedit blanditiis cum reprehenderit?</p>
+                            </v-flex>
+                            <v-flex xs4 class="text-xs-center">
+                                <v-icon size="65" class="mb-2">pets</v-icon>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sit quod id. Optio sint, earum quaerat laboriosam ab amet hic nesciunt, facilis exercitationem qui iusto ipsa impedit blanditiis cum reprehenderit?</p>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                    <v-divider></v-divider>
+
+                    <v-card-title primary-title class="align-center justify-center">
+                        <div>
+                            <h3 class="display-1">Contactanos</h3>
+                        </div>
+                    </v-card-title>
+                    <v-card-text>
+                        <p
+                            class="subheading text-xs-center"
+                        >Envianos un mail, contestaremos a la brevedad</p>
+                    </v-card-text>
+                    <v-container grid-list-xl>
+                        <v-layout row wrap>
+                            <v-flex xs6>
+                                <v-text-field label="Nombre"></v-text-field>
+                            </v-flex>
+                            <v-flex xs6>
+                                <v-text-field label="Email"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12>
+                                <v-textarea name label="Mensaje"></v-textarea>
+                            </v-flex>
+                        </v-layout>
+                        <v-card-actions class="align-center justify-center">
+                            <v-btn color="primary">Enviar</v-btn>
+                        </v-card-actions>
+                    </v-container>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
