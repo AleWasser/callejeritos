97280f573b5f67a34498dc9d3f62a9b449a48e92
Alejo Wasser <alejowasser@gmail.com>
2019-07-25 01:18:35 -0300

    Sidenav component

---
+++ components/Navigation/Sidenav.vue
@@ -0,0 +1,62 @@
+<template>
+    <nav>
+        <v-toolbar dense flat fixed light color="transparent">
+            <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+        </v-toolbar>
+        <v-navigation-drawer v-model="drawer" :mini-variant="mini" dark temporary app>
+            <v-list class="pa-1">
+                <v-list-tile v-if="mini" @click.stop="mini = !mini">
+                    <v-list-tile-action>
+                        <v-icon>chevron_right</v-icon>
+                    </v-list-tile-action>
+                </v-list-tile>
+
+                <v-list-tile avatar tag="div">
+                    <v-list-tile-content>
+                        <v-list-tile-title>Callejeritos Almirante Brown</v-list-tile-title>
+                    </v-list-tile-content>
+
+                    <v-list-tile-action>
+                        <v-btn icon @click.stop="mini = !mini">
+                            <v-icon>chevron_left</v-icon>
+                        </v-btn>
+                    </v-list-tile-action>
+                </v-list-tile>
+            </v-list>
+
+            <v-list class="pt-0">
+                <v-divider light></v-divider>
+                <nuxt-link
+                    tag="v-list-tile"
+                    v-for="item in items"
+                    :key="item.title"
+                    :to="item.link"
+                    @click
+                >
+                    <v-list-tile-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </nuxt-link>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: false,
+            mini: false,
+            items: [
+                { icon: "home", title: "Home", link: "/" },
+                { icon: "feedback", title: "Blog", link: "/blog" }
+            ]
+        };
+    }
+};
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,48 +1,6 @@
 <template>
     <v-app>
-        <v-toolbar dense flat fixed dark color="transparent">
-            <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-            <v-toolbar-title>Callejeritos Almirante Brown</v-toolbar-title>
-        </v-toolbar>
-        <v-navigation-drawer v-model="drawer" :mini-variant="mini" absolute dark temporary app>
-            <v-list class="pa-1">
-                <v-list-tile v-if="mini" @click.stop="mini = !mini">
-                    <v-list-tile-action>
-                        <v-icon>chevron_right</v-icon>
-                    </v-list-tile-action>
-                </v-list-tile>
-
-                <v-list-tile avatar tag="div">
-                    <v-list-tile-avatar>
-                        <img src="https://randomuser.me/api/portraits/men/85.jpg" />
-                    </v-list-tile-avatar>
-
-                    <v-list-tile-content>
-                        <v-list-tile-title>John Leider</v-list-tile-title>
-                    </v-list-tile-content>
-
-                    <v-list-tile-action>
-                        <v-btn icon @click.stop="mini = !mini">
-                            <v-icon>chevron_left</v-icon>
-                        </v-btn>
-                    </v-list-tile-action>
-                </v-list-tile>
-            </v-list>
-
-            <!-- <v-list class="pt-0" dense>
-                <v-divider light></v-divider>
-
-                <v-list-tile v-for="item in items" :key="item.title" @click>
-                    <v-list-tile-action>
-                        <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-tile-action>
-
-                    <v-list-tile-content>
-                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-            </v-list>-->
-        </v-navigation-drawer>
+        <app-sidenav></app-sidenav>
         <nuxt />
         <v-footer>
             <span>&copy; 2019</span>
@@ -51,12 +9,10 @@
 </template>
 
 <script>
+import Sidenav from "~/components/Navigation/Sidenav.vue";
 export default {
-    data() {
-        return {
-            drawer: false,
-            mini: false
-        };
+    components: {
+        "app-sidenav": Sidenav
     }
 };
 </script>
