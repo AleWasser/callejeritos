dbf017b37c9de96624ecce165c05907461f582f2
AleWasser <alejowasser@gmail.com>
2019-09-22 21:12:14 -0300

    Botones MercadoPago

--- pages/donaciones/index.vue
+++ pages/donaciones/index.vue
@@ -8,8 +8,29 @@
             <v-card>
                 <v-container fluid grid-list-md>
                     <v-layout row wrap>
+                        <v-card-title primary-title>
+                            <h3 class="display-1">MercadoPago</h3>
+                        </v-card-title>
                         <v-flex xs12 :class="{'my-1 pa-0': getBreakpoint.smAndDown}">
-                           
+                            <v-btn
+                                color="primary"
+                                href="https://www.mercadopago.com/mla/debits/new?preapproval_plan_id=4fc5f3860bf34947ababdf3a978d7c23"
+                                target="_blanck"
+                            >Subscribirse</v-btn>
+                            <v-btn
+                                color="primary"
+                                href="https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=214223563-8b4ed523-df66-4451-9acb-146d432ef03d"
+                                target="_blanck"
+                            >Donar $100</v-btn>
+                        </v-flex>
+                        <v-flex xs12 :class="{'my-1 pa-0': getBreakpoint.smAndDown}">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio adipisci rem aperiam, autem expedita reprehenderit similique nam quam, iste iure commodi? Animi, ipsam. Quidem repellendus molestias architecto esse obcaecati ipsa!</p>
+                        </v-flex>
+                        <v-flex xs12 :class="{'my-1 pa-0': getBreakpoint.smAndDown}" text-xs-center>
+                            <p>
+                                Tenes dudas?
+                                <nuxt-link to="#">contactanos</nuxt-link>
+                            </p>
                         </v-flex>
                     </v-layout>
                 </v-container>
