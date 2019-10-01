c8b5d6378180b8ab0b1b8eeaca68e9db407d1d8e
Alejo Wasser <alejowasser@amab.org.ar>
2019-10-01 16:01:53 -0300

    Validacion login

--- pages/admin/login/index.vue
+++ pages/admin/login/index.vue
@@ -8,7 +8,22 @@
                         <h3 class="headline mb-0">Login</h3>
                     </v-card-title>
                     <v-card-text>
-                        <v-text-field v-model="email" name="email" label="Email" id="email"></v-text-field>
+                        <span v-if="$v.email.$error" class="error--text caption">Campo requerido</span>
+                        <v-text-field
+                            v-model="email"
+                            name="email"
+                            label="Email"
+                            id="email"
+                            @blur="$v.email.$touch()"
+                            :error="$v.email.$error"
+                            required
+                            class="pt-1 mt-1"
+                            autocomplete="off"
+                        ></v-text-field>
+                        <span
+                            v-if="$v.password.$error"
+                            class="error--text caption"
+                        >Debes introducir una contraseña</span>
                         <v-text-field
                             v-model="password"
                             :append-icon="show ? 'visibility' : 'visibility_off'"
@@ -16,11 +31,21 @@
                             name="password"
                             label="Password"
                             @click:append="show = !show"
+                            @blur="$v.password.$touch()"
+                            :error="$v.password.$error"
+                            required
+                            class="pt-1 mt-1"
+                            autocomplete="off"
                         ></v-text-field>
                     </v-card-text>
                     <v-card-actions class="justify-center">
                         <v-btn flat color="primary" to="/">Cancelar</v-btn>
-                        <v-btn flat color="primary" type="submit">Ingresar</v-btn>
+                        <v-btn
+                            flat
+                            color="primary"
+                            type="submit"
+                            :disabled="this.$v.$invalid"
+                        >Ingresar</v-btn>
                     </v-card-actions>
                 </v-card>
             </form>
@@ -30,6 +55,7 @@
 
 <script>
 import { mapActions } from "vuex";
+import { required, email } from "vuelidate/lib/validators";
 
 import Notification from "~/components/Notification.vue";
 
@@ -49,9 +75,21 @@ export default {
             userLogin: "userLogin"
         }),
         onSubmit() {
-            this.userLogin({ email: this.email, password: this.password }).then(
-                () => this.$router.push("/admin")
-            );
+            if (!this.$v.$invalid) {
+                this.userLogin({
+                    email: this.email,
+                    password: this.password
+                }).then(() => this.$router.push("/admin"));
+            }
+        }
+    },
+    validations: {
+        email: {
+            required,
+            email
+        },
+        password: {
+            required
         }
     }
 };
